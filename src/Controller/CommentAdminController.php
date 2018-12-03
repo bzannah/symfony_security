7ade6db9633219d894d2f7c9fbed390444848c05
Babagana Zannah <babagana.tech@gmail.com>
2018-12-03 21:55:36 +0100

    add authorization with method and annotation

--- src/Controller/CommentAdminController.php
+++ src/Controller/CommentAdminController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Repository\CommentRepository;
 use Knp\Component\Pager\PaginatorInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
@@ -12,9 +13,13 @@ class CommentAdminController extends Controller
 {
     /**
      * @Route("/admin/comment", name="comment_admin")
+     * @IsGranted("ROLE_ADMIN")
      */
     public function index(CommentRepository $repository, Request $request, PaginatorInterface $paginator)
     {
+        // you can use annotation - you can put it above the class as well
+        //$this->denyAccessUnlessGranted('ROLE_ADMIN');
+
         $q = $request->query->get('q');
 
         $queryBuilder = $repository->getWithSearchQueryBuilder($q);
